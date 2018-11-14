3ebf8284dcf0b4824b0cb8cb20fc2fdd768b88a0
rundhik <randy.anwar@gmail.com>
2018-11-15 05:29:57 +0700

    Lara Multi Role Basic

---
+++ app/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace IBMTStudent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name', 'detail',
+    ];
+}
---
+++ database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $p = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete',
+        ];
+
+        foreach ($p as $r) {
+            Permission::create(['name' => $r]);
+        }
+    }
+}
